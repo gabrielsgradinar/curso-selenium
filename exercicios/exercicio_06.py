5201c8498bc5311ca91e95ade97ac557080ce494
Gabriel Gradinar <gabrielgradinar@hotmail.com>
2020-09-21 16:21:43 -0300

    Formataçao

--- exercicios/exercicio_06.py
+++ exercicios/exercicio_06.py
@@ -13,13 +13,18 @@ sleep(2)
 for i in range(0, 6):
     sleep(1)
 
-    form_para_preencher = browser.find_element_by_css_selector("header span").text
+    form_para_preencher = browser.find_element_by_css_selector(
+        "header span").text
 
-    form = browser.find_element_by_css_selector(f"form.form-{form_para_preencher}")
-    form.find_element_by_css_selector('input[name="nome"]').send_keys("Gabriel")
-    form.find_element_by_css_selector('input[name="senha"]').send_keys("1705")
+    form = browser.find_element_by_css_selector(
+        f"form.form-{form_para_preencher}")
+    form.find_element_by_css_selector(
+        'input[name="nome"]').send_keys("Gabriel")
+    form.find_element_by_css_selector(
+        'input[name="senha"]').send_keys("1705")
     sleep(1)
-    form.find_element_by_css_selector(f'input[name="{form_para_preencher}"]').click()
+    form.find_element_by_css_selector(
+        f'input[name="{form_para_preencher}"]').click()
 
 # sleep(2)
 # browser.quit()
